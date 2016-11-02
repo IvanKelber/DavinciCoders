feecb66f60953d481e978e4873658db1f3bfd7b7
Jeremy W <jeremymwatson@gmail.com>
2016-11-02 16:15:50 -0400

    testing loading images into tensors, not working

--- Parse/parser.py
+++ Parse/parser.py
@@ -13,9 +13,9 @@ class Parser:
         for era in os.listdir(path):
             if(not isfile(era)):
                 self.images += self.get_images(join(path,era))
+        print self.images
+        self.load_images_into_tensor_array(self.images)
 
-        print len(self.images)
-        
     def get_images(self,era):
         artists = [artist for artist in os.listdir(era) if not isfile(join(era,artist))]
         images = []
@@ -25,12 +25,36 @@ class Parser:
     def parse_image(image):
         pass
 
-    def load_images_into_tensor_array(image_location_tuple_list, dimension=DIMENSION):
+    def load_images_into_tensor_array(self, image_location_tuple_list, dimension=DIMENSION):
         """
         Return two arrays, first is array of TF image records, second is corresponding labels
         """
-        for img, location in image_location_tuple_list:
-            
+        image_files = [join(image_path,image) for image, image_path in image_location_tuple_list]
+        tf_queue = tf.train.string_input_producer(image_files)
+        reader = tf.WholeFileReader()
+        key, value = reader.read(tf_queue)
+
+        out_image = tf.image.decode_jpeg(value)
+
+        sess = tf.Session()
+        sess.as_default()
+        sess.run(tf.initialize_all_variables())
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+
+        tf_array = []
+
+        for i in range(len(image_files)):
+            curr_image = out_image.eval(session=sess)
+            tf_array.append(curr_image)
+
+        print len(tf_array)
+
+        coord.request_stop()
+        coord.join(threads)
+
+
 
 
 def main():
