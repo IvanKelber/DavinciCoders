6ff9f4346531ba9ef4e59ca8965ee8df75f487c2
joshcusano <joshcusano@gmail.com>
2016-11-17 00:04:14 -0500

    Altering RunAll to use new pixel generator

--- BasicLSTM/main.py
+++ BasicLSTM/main.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python
 import sys
 from process_image import ProcessedImage, StringsToBatches, StringsToImage
-from basic_LSTM_net import BasicLSTMNet
+from basic_LSTM_net import BasicLSTMNet, RunNet
 
 def RunAll():
-	"""Run python mina.py <name of image>"""
+	"""Run python main.py <name of image>"""
+	#process image
 	input_image = sys.argv[1]
 	new_img = ProcessedImage(input_image)
 	vocab_sz, pixelstr_ints, reversed_dict = StringsToBatches.words_into_ints(new_img.string_pixels)
-
-	simple_net = BasicLSTMNet(vocab_sz, .5, 50, 256, 20, 35, 15)
-	train_data, test_data = StringsToBatches.split_train_test(pixelstr_ints, .9)
-	train_batches = StringsToBatches.get_batches(train_data, simple_net.batch_size, simple_net.num_steps)
-	test_batches =  StringsToBatches.get_batches(test_data, simple_net.batch_size, simple_net.num_steps)
-	
-	simple_net.setup_net()
-	simple_net.train_net(train_batches)
-	simple_net.test_net(test_batches)
-
-	result_pixelstr = StringsToBatches.ints_to_pixelstr(reversed_dict, simple_net.final_pixels)
+	#set up training and generating nets
+	training_net = BasicLSTMNet("train", vocab_sz, .5, 50, 256, 20, 35, 15)
+	generating_net = BasicLSTMNet("generate", vocab_sz, 1.0, 50, 256, 1, 1)
+	#get train and test data
+	train_data, test_data = StringsToBatches.split_train_test(pixelstr_ints, .90)
+	train_batches = StringsToBatches.get_batches(train_data, training_net.batch_size, training_net.num_steps)
+	test_batches =  StringsToBatches.get_batches(test_data, training_net.batch_size, training_net.num_steps)
+	#train, test, and generate
+	run_nets = RunNet(training_net, generating_net, train_batches, test_batches, 75000)
+	run_nets.train_net()
+	run_nets.test_net()
+	run_nets.generate_image()
+	#create new image
+	result_pixelstr = StringsToBatches.ints_to_pixelstr(reversed_dict, run_nets.final_pixels)
 	pixels = [StringsToImage.string_pixel_to_pixel(w) for w in result_pixelstr]
 	StringsToImage.string_pixel_to_image(pixels, new_img.width)
 
