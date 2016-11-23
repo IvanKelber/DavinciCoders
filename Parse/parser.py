e61e6c28edd3d4ae758c0abe71a17cb605ff47e9
joshcusano <joshcusano@gmail.com>
2016-11-22 21:50:38 -0500

    added methods to process cifar-10. This is some scary bad code

--- Parse/parser.py
+++ Parse/parser.py
@@ -2,19 +2,29 @@ import tensorflow as tf
 import numpy as np
 import os
 from os.path import isfile, join
+import cPickle
+from PIL import Image
 
-
-DIMENSION = 64
-
+DIMENSION = 32
 class Parser:
 
-    def __init__(self,path):
+    def __init__(self,path, cifar_flag=False):
         self.images = []
-        for era in os.listdir(path):
-            if(not isfile(era)):
-                self.images += self.get_images(path,era)
-        #print self.images[0]
-        self.load_images_into_tensor_array(self.images)
+        self.final_images = []
+        if cifar_flag:
+            img_dict = self.unpickle(path)
+            for ix, label in enumerate(img_dict['labels']):
+                 if label == 0:
+                    self.final_images.append(self.get_cifar_img(img_dict['data'][ix]))
+            #Slice list so that it is a multiple of batch size, batch size must be square number
+            self.final_images = self.final_images[:1000]
+            self.final_images = np.reshape(self.final_images, (1000, -1))         
+        else:
+            for era in os.listdir(path):
+                if(not isfile(era)):
+                    self.images += self.get_images(path,era)
+            self.load_images_into_tensor_array(self.images)
+        
 
     def get_images(self,path,era):
         """
@@ -30,20 +40,46 @@ class Parser:
     def parse_jpg(jpg):
         return tf.image.decode_jpeg(jpg,channels=3,ratio=8)
 
+    def unpickle(self, path):
+        fo = open(path, 'rb')
+        dict = cPickle.load(fo)
+        fo.close()
+        return dict
 
+    def scaled_rgb_tuple_to_grayscale(self, img):
+        return round(int(round((.21 * img[0]) + (.72 * img[1]) + (.07 * img[2])))/255.0, 8)
+
+    def get_cifar_img(self, raw_array):
+        img = []
+        index_r = 0
+        index_g = 1024
+        index_b = 2048
+        while index_r < 1024:
+            tup = (raw_array[index_r], raw_array[index_g], raw_array[index_b])
+            gray = self.scaled_rgb_tuple_to_grayscale(tup)
+            img.append(gray)
+            index_r += 1
+            index_g += 1
+            index_b += 1
+        img = np.reshape(img, (1024, -1))
+        return img
+
+    def pixel_to_image(self, array, width, height):
+        imgfinal = Image.new("L", ((width, height)))
+        imgfinal.putdata(array)
+        imgfinal.save('testfinal.png')
+        print "Test image saved to testfinal.png"
 
     def load_images_into_tensor_array(self, image_location_tuple_list, dimension=DIMENSION):
         """
         Return two arrays, first is array of TF image records, second is corresponding labels
         """
-        print len(image_location_tuple_list)
         image_files = [image for image, _ in image_location_tuple_list]
-        print len(image_files)
         tf_queue = tf.train.string_input_producer(image_files)
         reader = tf.WholeFileReader()
         key, value = reader.read(tf_queue)
 
-        out_image = tf.image.resize_image_with_crop_or_pad(tf.image.decode_jpeg(value), DIMENSION, DIMENSION)
+        out_image = tf.image.rgb_to_grayscale(tf.image.resize_image_with_crop_or_pad(tf.image.decode_jpeg(value), DIMENSION, DIMENSION))
 
         sess = tf.Session()
         sess.as_default()
@@ -58,17 +94,19 @@ class Parser:
             curr_image = out_image.eval(session=sess)
             tf_array.append(curr_image)
 
-        print len(tf_array)
-
+        #print len(tf_array)
+        #unpack array, scale pixels
+        for i in tf_array:
+            img = list()
+            for p in i:
+                for x in p:
+                    img.append(float(p[0])/255)
+            self.final_images.append(img)
         coord.request_stop()
         coord.join(threads)
 
-
-
-
 def main():
-    p = Parser("Data/Pandora_18k/")
-
+    p = Parser("/gpfs/main/home/jcusano/course/cs2950k/draw/cifar-img/batches/data_batch_1", True)
 
 if __name__ == '__main__':
     main()
