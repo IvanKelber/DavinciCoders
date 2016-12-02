5e050ab56e3a4260eddbc2b18821171b4ab510cc
Ivan Kelber <ivankelber@gmail.com>
2016-12-02 15:03:45 -0500

    Changed some structure in runner.py.  Specifically, you can now call runner like 'python runner.py <arg1> <arg2>' where arg1 is the path to the training images and arg2 is the name of the folder you'd like to create to contain all of the trials

--- draw/runner.py
+++ draw/runner.py
@@ -5,23 +5,33 @@ import subprocess
 import sys
 import time
 
-img_dims =[256]
+img_dims =[64,256]
 batch_sizes = [64]
-iters=[1500]
-canvasses=[20,40]
+iters=[2000]
+canvasses=[20]
 source_dir = ""
+dest_dir = ""
 try:
 	source_dir = sys.argv[1]
 except IndexError:
 	source_dir="../11_Expressionism"
+
+try:
+	dest_dir = sys.argv[2]
+except IndexError:
+	dest_dir="../tmp"
+
 starting_string = "python draw.py --source_dir=%s\
  --read_attn=True --write_attn=True " % (source_dir)
 count = 0
+subprocess.call(["mkdir",(dest_dir)])
+
+dir_prefix= source_dir[6:9].lower()
 for dim in img_dims:
 	for size in batch_sizes:
 		for i in iters:
 			for canvas in canvasses:
-				data_dir = "script_test_64/exp_%d_%d_%d_%d" % (dim,size,i,canvas)
+				data_dir = "%s/%s_%d_%d_%d_%d" % (dest_dir,dir_prefix,dim,size,i,canvas)
 				subprocess.call(["mkdir",(data_dir)])
 				print "MADE DIRECTORY: %s" % data_dir
 				execution_string = starting_string + \
@@ -32,11 +42,11 @@ for dim in img_dims:
 				start = time.clock()
 				subprocess.call(execution_string.split(" "))
 				end = time.clock()
-				elapsed = abs(end-start) / 60.0
+				elapsed = abs(end-start)
 				print "Done drawing %d_%d_%d_%d in location: %s" %(dim,size,i,canvas,data_dir)
 				print "Elapsed Time: %d" % elapsed
 				count+=1
 				if(count%5==0):
-					subprocess.call(["python","plotter.py","./script_test_64"])
+					subprocess.call(["python","plotter.py",dest_dir])
 
-subprocess.call(["python","plotter.py","./script_test_64"])
+subprocess.call(["python","plotter.py",dest_dir])
