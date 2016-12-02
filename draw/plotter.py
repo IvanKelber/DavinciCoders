98be915d411b1ff1202a427ff84f6364ed4a2f65
Ivan Kelber <ivankelber@gmail.com>
2016-12-02 14:33:18 -0500

    current

--- draw/plotter.py
+++ draw/plotter.py
@@ -10,7 +10,7 @@ os.chdir(root)
 for directory in os.listdir(os.getcwd()):
 	if(not os.path.isfile(directory)):
 		os.chdir(directory)
-		plot_string = "python ../../plot_data.py noattn draw_data.npy"
+		plot_string = "python ../../plot_data.py mogged draw_data.npy"
 		subprocess.call(plot_string.split(" "))
 		os.chdir("..")
 
--- draw/runner.py
+++ draw/runner.py
@@ -2,26 +2,41 @@
 
 #(img_dim,batch_size,iters)
 import subprocess
+import sys
+import time
 
-img_dims =[32,64,128,512]
-batch_sizes = [64,25,9]
-iters=[1000,2000]
-
-starting_string = "python draw.py --source_dir=../11_Expressionism\
- --read_attn=True --write_attn=True "
+img_dims =[256]
+batch_sizes = [64]
+iters=[1500]
+canvasses=[20,40]
+source_dir = ""
+try:
+	source_dir = sys.argv[1]
+except IndexError:
+	source_dir="../11_Expressionism"
+starting_string = "python draw.py --source_dir=%s\
+ --read_attn=True --write_attn=True " % (source_dir)
 count = 0
 for dim in img_dims:
 	for size in batch_sizes:
 		for i in iters:
-			data_dir = "script_test/exp_%d_%d_%d" % (dim,size,i)
-			subprocess.call(["mkdir",(data_dir)])
-			execution_string = starting_string + \
-			"--data_dir=%s --img_dims=%d\
-			 --batch_sizes=%d --iters=%d" %(data_dir,dim,size,i)
-			subprocess.call(execution_string.split(" "))
-			print "Done drawing %d_%d_%d in location: %s" %(dim,size,i,data_dir)
-			count+=1
-			if(count%5==0):
-				subprocess.call(["python","plotter.py","./script_test"])
+			for canvas in canvasses:
+				data_dir = "script_test_64/exp_%d_%d_%d_%d" % (dim,size,i,canvas)
+				subprocess.call(["mkdir",(data_dir)])
+				print "MADE DIRECTORY: %s" % data_dir
+				execution_string = starting_string + \
+				"--data_dir=%s --img_dim=%d\
+				 --batch_size=%d --iters=%d\
+				 --canvasses=%d" %(data_dir,dim,size,i,canvas)
+				print "==== Populating Directory ==="
+				start = time.clock()
+				subprocess.call(execution_string.split(" "))
+				end = time.clock()
+				elapsed = abs(end-start) / 60.0
+				print "Done drawing %d_%d_%d_%d in location: %s" %(dim,size,i,canvas,data_dir)
+				print "Elapsed Time: %d" % elapsed
+				count+=1
+				if(count%5==0):
+					subprocess.call(["python","plotter.py","./script_test_64"])
 
-subprocess.call(["python","plotter.py","./script_test"])
+subprocess.call(["python","plotter.py","./script_test_64"])
